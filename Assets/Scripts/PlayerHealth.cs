30f7f738cbde883edcb4139a74aef047c3e15aa2
Jacob Aberasturi <aberasturi.j@northeastern.edu>
2024-05-28 16:38:42 -0400

    First WIP new enemys
    made Gunner and started thumper

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    // public AudioClip deathSFX;
+    public Slider healthSlider;
+
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth -= damage;
+            healthSlider.value = currentHealth;
+        }
+        if (currentHealth <= 0)
+        {
+            PlayerDies();
+        }
+
+        Debug.Log("Player health is: " + currentHealth);
+    }
+
+    public void Heal(int health)
+    {
+        if (currentHealth < 100)
+        {
+            currentHealth += health;
+            healthSlider.value = Mathf.Clamp(currentHealth, 0, 100);
+        }
+
+        Debug.Log("Player health with loot: " + currentHealth);
+    }
+
+    void PlayerDies()
+    {
+        Debug.Log("Player is dead");
+        // AudioSource.PlayClipAtPoint(deathSFX, transform.position);
+        transform.Rotate(-90, 0, 0, Space.Self);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("ProjectileEnemy"))
+        {
+            TakeDamage(other.gameObject.GetComponent<ProjectileAttributes>().projectileDamage);
+        }
+    }
+}
---
+++ PARASITE_Group5-Jarod/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 10f;
+
+    // public float minDistance = 0f;
+
+    public int damageAmount = 20;
+
+    private static int enemyCount = 0;
+
+    void Start()
+    {
+        enemyCount++;
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = moveSpeed * Time.deltaTime;
+
+        transform.LookAt(player);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damageAmount);
+            other.gameObject.GetComponent<CharacterController>().Move(-other.transform.forward * 2);
+        }
+        Invoke("Inertia", 0.5f);
+    }
+
+    //Stops enemy from skating away in a direction after a heavy collsion or bounce
+    void Inertia(){
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
