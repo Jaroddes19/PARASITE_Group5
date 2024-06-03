700f20ca1b75aa671ff69e47b461f8c05245647f
JarodD19 <jaroddes19@gmail.com>
2024-06-03 14:43:04 -0400

    Merging changes

--- Assets/Scripts/EnemyHit.cs
+++ Assets/Scripts/EnemyHit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,22 @@ public class EnemyHit : MonoBehaviour
     public GameObject bloodSplatter;
     public Slider healthSlider;
 
-    public int enemyHealth = 80;
-
-    int currentHealth;
+    CharacterAttributes charAttrs;
 
     void Start()
     {
-        currentHealth = enemyHealth;
-        healthSlider.value = currentHealth;
+        charAttrs = gameObject.GetComponentInParent<CharacterAttributes>();
+        charAttrs.currentHealth = charAttrs.maxHealth;
+
+        if (healthSlider == null)
+        {
+            healthSlider = gameObject.GetComponentInChildren<Slider>();
+        }
+        healthSlider.value = charAttrs.currentHealth;
+        if (bloodSplatter == null)
+        {
+            bloodSplatter = Resources.Load("BloodSplatter") as GameObject;
+        }
     }
 
     // Update is called once per frame
@@ -28,35 +37,41 @@ public class EnemyHit : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Projectile"))
         {
+            if (charAttrs.currentHealth > 0)
+            {
+                TakeDamage(other.gameObject.GetComponent<ProjectileAttributes>().projectileDamage, other);
+            }
             Destroy(other.gameObject);
-            if (currentHealth > 0)
-            {
-                TakeDamage(other.gameObject.GetComponent<ProjectileAttributes>().projectileDamage);
-
-            }
-            if (currentHealth <= 0)
-            {
-                DestroyEnemy();
-            }
-        }
-    }
-    public void TakeDamage(int damage)
-    {
-        if (currentHealth > 0)
-        {
-            currentHealth -= damage;
-            healthSlider.value = currentHealth;
-            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        }
-        if (currentHealth <= 0)
-        {
-            DestroyEnemy();
         }
     }
 
-    void DestroyEnemy()
+    /*
+     * Apply damage to the enemy. Accepts the collision object as well to provide
+     * additional damage effects
+     */
+    public void TakeDamage(int damage, Collision collision = null)
     {
-        Instantiate(bloodSplatter, transform.position, transform.rotation);
+        if (charAttrs.currentHealth > 0)
+        {
+            charAttrs.currentHealth -= damage;
+            healthSlider.value = charAttrs.currentHealth;
+            // Removed because these could push the enemy through walls and other colliders
+            //transform.position -= transform.forward * 2f
+            //gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (collision != null)
+            {
+                gameObject.GetComponent<Rigidbody>().AddForce(collision.contacts[0].normal * 2, ForceMode.Impulse);
+            }
+        }
+        if (charAttrs.currentHealth <= 0)
+        {
+            DestroySelf();
+        }
+    }
+
+    void DestroySelf()
+    {
+        Instantiate(bloodSplatter, transform.position, transform.rotation).SetActive(true);
         gameObject.SetActive(false);
         Destroy(gameObject, 0.5f);
     }
